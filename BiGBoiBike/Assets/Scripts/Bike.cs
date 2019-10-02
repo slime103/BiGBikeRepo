bfed41d792d2bc81796bd44b8023465dc69b6e00
Zman <zes888@gmail.com>
2019-10-01 20:12:11 -0400

    Controls
    Begins

---
+++ BiGBoiBike/Assets/Scripts/Bike.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -speed, ForceMode.Force);
+        }
+    }
+}
--- BiGBoiBike/Assets/Scripts/BikeController.cs
+++ BiGBoiBike/Assets/Scripts/BikeController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BikeController : MonoBehaviour
 {
+
+    public float turnSpeed = 1;
+    public float lerpSpeed = 0.7f;
+    public float angleLimit = 30;
+
+    public Rigidbody rb;
+
+    public float angleTraveled;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            angleTraveled += turnSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, angleTraveled, 0);
+            if (transform.eulerAngles.y > angleLimit)
+            {
+                transform.eulerAngles = new Vector3(0, angleLimit, 0);
+            }
+        }
     }
 }
